f7bc82e6f185cd9fe93a4dddc17b5f11c8474912
Garet Eliot <self@garet.io>
2018-09-10 13:34:53 -0500

    change body text color

--- src/themes/default.js
+++ src/themes/default.js
@@ -2,8 +2,9 @@ export default {
   color_primary: "#F688AD",
 
   body_background: "#fff",
-  body_color: "#000",
+  body_color: "inherit",
   body_margin: "0 2em",
 
   prism_background: "#fff",
+  prism_inline_color: "#000",
 };
