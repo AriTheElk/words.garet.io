f186f4c6bb7dde6d07b6331ece949e4828572391
Garet Eliot <self@garet.io>
2018-09-10 16:44:00 -0500

    change soundcloud player color

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
             options: {
               width: "100%",
               height: 400,
-              color: "#ff00ff",
+              color: "#F688AD",
             },
           },
           {
