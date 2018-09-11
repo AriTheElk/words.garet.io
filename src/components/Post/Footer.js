0482998ee136f2358032fe86d143915e3e032dc5
Garet Eliot <self@garet.io>
2018-09-10 19:12:15 -0500

    add more detail to the post footer

--- src/components/Post/Footer.js
+++ src/components/Post/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Link from "components/Link";
 
 const Footer = () => (
   <div>
+    <hr />
+    <p>
+      <i>
+        If you like what I've written or what I build and you think I'd be a
+        good fit for your team, drop me an email:{" "}
+        <Link href="mailto:hi@garet.io">hi@garet.io</Link>. I'm currently
+        seeking full-time or contract work in anything related to front-end
+        development.
+      </i>
+    </p>
     <p>Thanks for reading,</p>
     <p>—Garet</p>
   </div>
