2ab1e2cc4b8a442553d8793e4d4cd5fe41732753
Garet McKinley <self@garet.io>
2018-09-12 15:10:37 -0500

    add Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,9 +4,23 @@ import { transparentize, darken } from "polished";
 const Badge = styled.a`
   display: inline-block;
   padding: 4px 12px;
-  background: ${props => transparentize(0.75, props.theme.color_primary)};
-  color: ${props => darken(0.05, props.theme.color_primary)};
+  background: ${props => transparentize(0.75, props.theme.color_secondary)};
+  color: ${props => darken(0.05, props.theme.color_secondary)};
   font-size: ${props => props.theme.badge_font_size};
+  transition: all 0.35s;
+  border-radius: 4px;
+  backface-visibility: hidden;
+  transform: translateZ(0);
+
+  a:hover & {
+    color: ${props => darken(0.05, props.theme.color_secondary)};
+  }
+
+  :hover {
+    transform: scale(1.05);
+    box-shadow: 0 10px 15px
+      ${props => transparentize(0.9, darken(0.5, props.theme.color_secondary))};
+  }
 `;
 
 export default Badge;
